fce738010256d140af632f52b499a6f94ed9e8e6
vagrant <thecarloferrer@gmail.com>
2017-09-11 17:42:05 +0000

    Can make requests to server.

--- download_avatars.js
+++ download_avatars.js
@@ -9,6 +9,15 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
   console.log(requestURL); // debugging
+
+  request.get(requestURL)
+    .on('error', function(err) {
+      throw err;
+    })
+    .on('response', function(response) {
+      console.log('Response Status Code: ', response.statusCode);
+    console.log('Headers / Content Type: ', response.headers['content-type']);
+    });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
