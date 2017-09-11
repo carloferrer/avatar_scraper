64b08b7c75e2fa89ab41f3f418953de45c473b57
vagrant <thecarloferrer@gmail.com>
2017-09-11 17:35:53 +0000

    getRepoContributors initializes variable to hold URL string for requesting.

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,17 @@
+var GITHUB_USER = 'carloferrer';
+var GITHUB_TOKEN = '25a869c0dd19873ee0ff69b34be93bcd8c901096';
+
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos' + repoOwner + '/' + repoName + '/contributors';
+
+  // console.log(requestURL); // debugging
 }
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
